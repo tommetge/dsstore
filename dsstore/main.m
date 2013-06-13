a32a2fa7410ff39daa3a291923a7020dd309fc06
tom metge <tom@accident-prone.com>
2013-06-13 14:44:50 -0600

    Initial Commit

---
+++ dsstore/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dsstore
+//
+//  Created by Tom Metge on 6/13/13.
+//  Copyright (c) 2013 Flying Paper Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
